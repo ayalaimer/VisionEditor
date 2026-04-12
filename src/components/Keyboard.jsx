ad8e5e44517a32864ba35e31f82cb9d335af89d2
ayalaimer <ayalacohen456@gmail.com>
2026-04-12 18:14:57 +0300

    fixed

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -2,7 +2,7 @@
 function Keyboard({ language, onKeyPress, onDeleteChar, disabled }) {
   return (
     <div className={`keyboard ${disabled ? 'keyboard-disabled' : ''}`}>
-      {/* TODO: Partner needs to implement the keyboard layout and character buttons here. */}
+      {/* TODO: Implement the keyboard layout and character buttons here. */}
     </div>
   )
 }
